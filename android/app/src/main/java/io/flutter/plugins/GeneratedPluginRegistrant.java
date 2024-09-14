7446cb18ede912ce3c1752a1c317681d9b7fba9d
ss3414 <3414523068@qq.com>
2024-09-14 17:21:41 +0800

    first commit

---
+++ android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
@@ -0,0 +1,14 @@
+package io.flutter.plugins;
+
+import androidx.annotation.Keep;
+import androidx.annotation.NonNull;
+
+import io.flutter.embedding.engine.FlutterEngine;
+
+@Keep
+public final class GeneratedPluginRegistrant {
+    private static final String TAG = "GeneratedPluginRegistrant";
+
+    public static void registerWith(@NonNull FlutterEngine flutterEngine) {
+    }
+}
